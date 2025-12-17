ee61ff3c540a38c50402515945268cfcf1f0a4df
tampopo-studio <emicitti@gmail.com>
2025-12-17 17:19:42 +0100

    Added new features: date, time, temperature detail, wind speed and humidity

--- src/index.js
+++ src/index.js
@@ -1,11 +1,62 @@
 function showTemperature(response) {
   let temperatureToDisplay = document.querySelector("#temperature");
   let newTemperature = response.data.temperature.current;
+
   let cityToDisplay = document.querySelector("#city-name");
+  let descriptionToDisplay = document.querySelector("#description");
+  let humidityToDisplay = document.querySelector("#humidity");
+  let windSpeedToDisplay = document.querySelector("#wind-speed");
+  let newWindSpeed = Math.round(response.data.wind.speed);
+
+  let timeToDisplay = document.querySelector("#time");
+
+  let date = new Date(response.data.time * 1000);
+  console.log(date);
+
+  console.log(response.data);
 
   cityToDisplay.innerHTML = response.data.city;
+  timeToDisplay.innerHTML = formatDateAndTime(date);
+  windSpeedToDisplay.innerHTML = `${newWindSpeed}KM/H`;
+  humidityToDisplay.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionToDisplay.innerHTML = response.data.condition.description;
 
   temperatureToDisplay.innerHTML = Math.round(newTemperature);
+
+  console.log(timeToDisplay);
+}
+
+function formatDateAndTime(date) {
+  let year = date.getFullYear();
+  let month = 1 + date.getMonth();
+  let actualDate = date.getDate();
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (actualDate < 10) {
+    actualDate = `0${actualDate}`;
+  }
+
+  return `${actualDate}/${month}/${year} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
