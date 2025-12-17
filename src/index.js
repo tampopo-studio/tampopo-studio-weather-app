88e8b9a4d3ed6237e16a5e8f52d93b8f721859c1
tampopo-studio <emicitti@gmail.com>
2025-12-17 15:20:11 +0100

    Added API functions for city name and temperature display

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
+function showTemperature(response) {
+  let temperatureToDisplay = document.querySelector("#temperature");
+  let newTemperature = response.data.temperature.current;
+  let cityToDisplay = document.querySelector("#city-name");
+
+  cityToDisplay.innerHTML = response.data.city;
+
+  temperatureToDisplay.innerHTML = Math.round(newTemperature);
+}
+
 function searchCity(city) {
   let apiKey = "027a944a8237bdeb2dcbfffteb55oca7";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric
-`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
 }
 
 function searchEnteredCity(event) {
   event.preventDefault();
   let newCity = document.querySelector("#text-field");
-  let cityToDisplay = document.querySelector("#city-name");
-  cityToDisplay.innerHTML = newCity.value;
+
   searchCity(newCity.value);
 }
 
 let city = document.querySelector("#search-form");
 city.addEventListener("submit", searchEnteredCity);
+
+searchCity("Tokyo");
