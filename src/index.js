b688511d5eaf43746fbfff22c2b0d3729b9dc35f
tampopo-studio <emicitti@gmail.com>
2025-12-17 14:36:45 +0100

    added API url

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+function searchCity(city) {
+  let apiKey = "027a944a8237bdeb2dcbfffteb55oca7";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric
+`;
+}
+
 function searchEnteredCity(event) {
   event.preventDefault();
   let newCity = document.querySelector("#text-field");
   let cityToDisplay = document.querySelector("#city-name");
   cityToDisplay.innerHTML = newCity.value;
-  console.log(cityToDisplay);
+  searchCity(newCity.value);
 }
 
 let city = document.querySelector("#search-form");
